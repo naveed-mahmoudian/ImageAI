8256728e2c94143759c1a029ed18051f77e1c7ae
Naveed Mahmoudian <nmahmoudian@gmail.com>
2023-03-27 10:28:19 -0700

    Setting up home page and created core components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,27 @@
 import React, { useState, useEffect } from "react";
+import { Loader, Card, FormField } from "../components";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          Community Showcase
+        </h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">
+          Browse through a collection of other visually stunning images from the
+          community. Built with OpenAI's DALL-E AI.
+        </p>
+      </div>
+
+      <div className="mt-16">
+        <FormField />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
