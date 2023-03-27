74d3f930571b9870238631e18ab681352eaefb4a
Naveed Mahmoudian <nmahmoudian@gmail.com>
2023-03-27 12:21:09 -0700

    Created basic routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./mongoDB/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import aiRoutes from "./routes/aiRoutes.js";
 
 dotenv.config();
 
@@ -10,6 +12,8 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/ai", aiRoutes);
 
 app.get("/", async (req, res) => {
   res.send("Welcome to ImageAI by Nav");
