f281f31736f45af0918c65f4993e6903d324c32d
Naveed Mahmoudian <nmahmoudian@gmail.com>
2023-03-28 11:51:51 -0700

    Changed post route name

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/posts", {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
